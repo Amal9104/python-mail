bec8aa52033e3e10766dcef29d3cb095bb9b044d
unknown <mage1k99@gmail.com>
2020-04-24 20:05:09 +0530

    added BulkMail module

--- mail_module/sendBulkMail.py
+++ mail_module/sendBulkMail.py
@@ -1,4 +1,5 @@
 import email, smtplib, ssl
+import csv
 import getpass
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
@@ -16,4 +17,36 @@ if __name__ == "__main__":
     sendBulkMail(sender_mail_id=sender_email,sender_password=sender_password,smtp_address=smtp_address,smtp_port=smtp_port,subject=subject_mail,message=body_mail)
 
 def sendBulkMail(sender_mail_id,sender_password,smtp_address,smtp_port,subject,message):
-    csvfile = str(input("name of CSV File containing EmailIds of reciver"))
+    csvfile = str(input("name / path of CSV File containing EmailIds of reciver with extension"))
+    mail_address = []
+
+    try:
+        with open(csvfile,mode='r') as send_list:
+            csv_reader = csv.reader(csvfile,delimiter=',')
+            for row in csv_reader:
+                for mail in row:
+                    mail_address.append(mail)
+            send_list.close()
+
+    except FileNotFoundError as identifier:
+        print(identifier.strerror)
+    
+    mimeMessage = MIMEMultipart()
+    mimeMessage["From"] = sender_mail_id
+    mimeMessage["To"] = ','.join(mail_address)
+    mimeMessage["Subject"] = subject
+    mimeMessage.attach(MIMEText(message,'plain'))
+    mail_message = mimeMessage.as_string()
+
+    mServer = smtplib.SMTP(host=smtp_address,port=smtp_port)
+    mServer.starttls()
+
+    try:
+        mServer.login(user=sender_mail_id,password=sender_password)
+        mServer.sendmail(sender_mail_id,mail_address,mail_message)
+        print("mail Sent Successfully !")
+        mServer.quit()
+    except smtplib.SMTPException as identifier:
+        print(identifier.strerror)
+        print("Failed to Send Bulk Mail !")
+        mServer.quit()
